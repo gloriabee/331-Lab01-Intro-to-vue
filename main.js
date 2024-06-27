7ec9dc9d619523621935a3ba5c3c52df9ba8e325
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-06-27 12:21:24 +0700

    adding new link to product name

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ createApp({
     setup(){
         const product=ref('Boots');
         const description=ref('Fashionable, Comfort ones')
+        const image=ref('./assets/images/socks_green.jpg');
+        const url=ref('https://www.camt.cmu.ac.th');
         return{
-            product,description
+            product,
+            description,
+            image,
+            url
         }
     }
 }).mount('#app');
