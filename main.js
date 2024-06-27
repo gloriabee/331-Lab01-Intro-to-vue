5675421f4aa677a47a5abec735df472179e8c5ae
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-06-27 16:59:31 +0700

    adding onSale attribute

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ createApp({
         const description=ref('Fashionable, Comfort ones')
         const image=ref('./assets/images/socks_green.jpg');
         const url=ref('https://www.camt.cmu.ac.th');
+        const inStock=ref(false);
+        const inventory=ref(5);
+        const onSale=ref(false);
+
+
         return{
             product,
             description,
             image,
-            url
+            url,
+            inStock,
+            inventory,
+            onSale
         }
     }
 }).mount('#app');
