6d52130733d1a5e9a06b3c3cdf1291f8eb80bb93
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-06-27 18:40:12 +0700

    adding computed property for onSale status

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-const {createApp, ref}=Vue
+const {createApp, ref, computed}=Vue
 createApp({
     setup(){
         const product=ref('Boots');
+        const brand=ref('SE 331');
         const description=ref('Fashionable, Comfort ones')
-        const image=ref('./assets/images/socks_green.jpg');
+       
         const url=ref('https://www.camt.cmu.ac.th');
-        const inStock=ref(false);
+       
         const inventory=ref(50);
-        const onSale=ref(false);
+        const onSale=ref(true);
         const details=ref([
             '50% cotton',
             '30% wool',
             '20% polyester'
         ])
         const variants=ref([
-            {id:2234, color:'green',image:'./assets/images/socks_green.jpg'},
-            {id:2235, color:'blue',image:'./assets/images/socks_blue.jpg'}
+            {id:2234, color:'green',image:'./assets/images/socks_green.jpg',quantity:50},
+            {id:2235, color:'blue',image:'./assets/images/socks_blue.jpg',quantity:0}
         ])
+        const selectedVariant=ref(0);
+        function updateVariant(index){
+            selectedVariant.value=index;
+        }
+        const image=computed(()=>{
+            return variants.value[selectedVariant.value].image;
+        })
+        const inStock=computed(()=>{
+            return variants.value[selectedVariant.value].quantity
+        })
         const sizes=ref([
             'S',
             'M',
@@ -26,6 +37,19 @@ createApp({
         function addToCart(){
             cart.value+=1;
         }
+
+        const title=computed(()=>{
+            return brand.value + ' '+product.value
+        })
+
+        const saleStatus=computed(()=>{
+            if(onSale.value){
+                return brand.value+' '+product.value+' is on sale';
+            }
+            else{
+                return '';
+            }
+        })
 
         function updateImage(variantImage){
             image.value=variantImage;
@@ -38,6 +62,8 @@ createApp({
 
         return{
             product,
+            brand,
+            title,
             description,
             image,
             url,
@@ -48,9 +74,12 @@ createApp({
             variants,
             sizes,
             cart,
+            saleStatus,
             addToCart,
             updateImage,
-            toggleStock
+            toggleStock,
+            updateVariant,
+           
         }
     }
 }).mount('#app');
